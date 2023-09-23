c7190cb9bd35efcbebf3d42826f3b57bbf0e36c0
Autumn <batorkittiescoding@gmail.com>
2023-09-23 15:12:35 -0400

    learning functions

--- functions.js
+++ functions.js
@@ -75,3 +75,65 @@ function truncateString(str, num) {
   return str.slice(0, num) + "...";
 }
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8)); //this will print A-tisket...
+
+//A common pattern in JavaScript is to execute a function as soon as it is declared:
+(function () {
+  console.log("Chirp, chirp!");
+})();
+//This is an anonymous function expression that executes right away, and outputs Chirp, chirp! immediately.
+
+//Note that the function has no name and is not stored in a variable. The two parentheses () at the end of the function expression cause it to be immediately executed or invoked. This pattern is known as an immediately invoked function expression or IIFE.
+
+//Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
+(function () {
+  console.log("A cozy nest is ready");
+})();
+//this will print A cozy nest is ready
+
+//An immediately invoked function expression (IIFE) is often used to group related functionality into a single object or module. For example, an earlier challenge defined two mixins:
+function glideMixin(obj) {
+  obj.glide = function () {
+    console.log("Gliding on the water");
+  };
+}
+function flyMixin(obj) {
+  obj.fly = function () {
+    console.log("Flying, wooosh!");
+  };
+}
+//We can group these mixins into a module as follows:
+let motionModule = (function () {
+  return {
+    glideMixin: function (obj) {
+      obj.glide = function () {
+        console.log("Gliding on the water");
+      };
+    },
+    flyMixin: function (obj) {
+      obj.fly = function () {
+        console.log("Flying, wooosh!");
+      };
+    },
+  };
+})();
+//Note that you have an immediately invoked function expression (IIFE) that returns an object motionModule. This returned object contains all of the mixin behaviors as properties of the object. The advantage of the module pattern is that all of the motion behaviors can be packaged into a single object that can then be used by other parts of your code. Here is an example using it:
+motionModule.glideMixin(duck);
+duck.glide(); // prints "Gliding on the water"
+
+//Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+let funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      //this is a mixin
+      obj.isCute = function () {
+        return true; //this will return true
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log("Singing to an awesome tune");
+      };
+    },
+  };
+})();
+//this will print Singing to an awesome tune
