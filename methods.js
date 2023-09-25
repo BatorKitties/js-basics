39c5a9c09c1fd5700d26414dece832fd53e8508f
Autumn <batorkittiescoding@gmail.com>
2023-09-25 13:26:25 -0400

    learning methods

--- methods.js
+++ methods.js
@@ -269,3 +269,206 @@ function incrementer() {
 var newValue = incrementer(); //should equal 5
 console.log(fixedValue); //should print 4
 //the newValue is the new value of the fixedValue
+
+//another useful array function is the filter method Array.prototype.filter() or simply filter()
+//filter calls a function on each element of an array and returns a new array containing only the elements for which that function returns a truthy value - that is, a value which returns true if passed to the Boolean() constructor.
+// In other words, it filters the array, based on the function passed to it.
+// Like map, it does this without needing to modify the original array.
+//The callback function accepts three arguments.
+//The first argument is the current element being processed.
+//The second is the index of that element and the third is the array upon which the filter method was called.
+//See below for an example using the filter method on the users array to return a new array containing only the users under the age of 30.
+// For simplicity, the example only uses the first argument of the callback.
+const users = [
+  { name: "John", age: 34 },
+  { name: "Amy", age: 20 },
+  { name: "camperCat", age: 10 },
+];
+const usersUnder30 = users.filter((user) => user.age < 30);
+console.log(usersUnder30); //this will print [{ name: "Amy", age: 20 }, { name: "camperCat", age: 10 }]
+
+//the variable watchList holds an array of objects with information on several movies.
+//Use a combination of filter and map on watchList to assign a new array of objects with only title and rating keys.
+//The new array should only include objects where imdbRating is greater than or equal to 8.0.
+//Note that the rating values are saved as strings in the object and you may need to convert them into numbers to perform mathematical operations on them.
+//example
+const watchList = [
+  //this is the array of objects
+  {
+    Title: "Inception",
+    Year: "2010",
+    Rated: "PG-13",
+    Released: "16 Jul 2010",
+    Runtime: "148 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer: "Christopher Nolan",
+    Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    Plot: "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    Language: "English, Japanese, French",
+    Country: "USA, UK",
+    Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.8",
+    imdbVotes: "1,446,708",
+    imdbID: "tt1375666",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Interstellar",
+    Year: "2014",
+    Rated: "PG-13",
+    Released: "07 Nov 2014",
+    Runtime: "169 min",
+    Genre: "Adventure, Drama, Sci-Fi",
+    Director: "Christopher Nolan",
+    Writer: "Jonathan Nolan, Christopher Nolan",
+    Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    Language: "English",
+    Country: "USA, UK",
+    Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.6",
+    imdbVotes: "910,366",
+    imdbID: "tt0816692",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "The Dark Knight",
+    Year: "2008",
+    Rated: "PG-13",
+    Released: "18 Jul 2008",
+    Runtime: "152 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer:
+      "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    Language: "English, Mandarin",
+    Country: "USA, UK",
+    Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    Metascore: "82",
+    imdbRating: "9.0",
+    imdbVotes: "1,652,832",
+    imdbID: "tt0468569",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Batman Begins",
+    Year: "2005",
+    Rated: "PG-13",
+    Released: "15 Jun 2005",
+    Runtime: "140 min",
+    Genre: "Action, Adventure",
+    Director: "Christopher Nolan",
+    Writer:
+      "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    Plot: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    Language: "English, Urdu, Mandarin",
+    Country: "USA, UK",
+    Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+    Metascore: "70",
+    imdbRating: "8.3",
+    imdbVotes: "972,584",
+    imdbID: "tt0372784",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Avatar",
+    Year: "2009",
+    Rated: "PG-13",
+    Released: "18 Dec 2009",
+    Runtime: "162 min",
+    Genre: "Action, Adventure, Fantasy",
+    Director: "James Cameron",
+    Writer: "James Cameron",
+    Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    Language: "English, Spanish",
+    Country: "USA, UK",
+    Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    Metascore: "83",
+    imdbRating: "7.9",
+    imdbVotes: "876,575",
+    imdbID: "tt0499549",
+    Type: "movie",
+    Response: "True",
+  },
+];
+
+//in order to filter and map to only get the title and rating keys that have an imdbRating greater than or equal to 8.0
+//we would do this
+const filteredList = watchList
+  .map((item) => ({
+    //this is the map method
+    title: item["Title"], //this is the title key
+    rating: item["imdbRating"],
+  }))
+  .filter((item) => item.rating >= 8.0); //this is the filter method
+//the map method is used to map the array
+//the filter method is used to filter the array
+//the item is the current value of the array
+//the filteredList is the new array
+//the title and rating are the keys of the objects
+//the imdbRating is the value of the rating key
+//the 8.0 is the value of the rating key
+//the () is the callback function which is used to filter and map the array
+//the {} is the object so the title and rating are the keys of the object
+//the [] is the array so the watchList is the array
+//the => is the arrow function so the item is the current value of the array
+
+//theres also a map method
+//the map method is used to map the array
+//for example
+const array = [1, 2, 3, 4, 5];
+const newArrayy = array.map((val) => val + 3);
+console.log(newArrayy); //this will print [4, 5, 6, 7, 8] because it adds 3 to each element in the array
+//so 1 would be 4, 2 would be 5, 3 would be 6, 4 would be 7, 5 would be 8
+//the val is the current value of the array
+//the newArrayy is the new array
+//the numbers in the array are the elements of the array
+//so all map does is it takes an array and returns a new array
+//it can be used in place of a for loop
+
+//you can use methods before things they dont have to be written after
+//for example
+const squareList = (arr) => {
+  //this is a function declaration
+  //this is a function that takes an array as an argument
+  //this is a function that returns a new array with only the positive integers which are squared
+  //this is a function that uses the filter and map methods
+  const squaredIntegers = arr
+    .filter((num) => num > 0 && num % parseInt(num) === 0)
+    .map((num) => Math.pow(num, 2));
+  return squaredIntegers;
+};
+
+//instead of
+const squareLIst = (arr) => {
+  //this is a function declaration
+  //this is a function that takes an array as an argument
+  //this is a function that returns a new array with only the positive integers which are squared
+  //this is a function that uses the filter and map methods
+  const squaredINtegers = arr.filter(
+    (num) => num > 0 && num % parseInt(num) === 0
+  );
+  const squaredIntegers = squaredIntegers.map((num) => Math.pow(num, 2));
+  return squaredIntegers;
+};
